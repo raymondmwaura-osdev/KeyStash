693b67d0bda521e8fea39402ea8e035653c9e547
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2025-11-08 13:05:33 +0300

    feat: Add command-line options for the 'add' feature.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,27 @@
-#!/bin/python3
-
 from features import add
 import argparse
 
 parser = argparse.ArgumentParser(prog="KeyStash")
 subparsers = parser.add_subparsers(dest="cmd")
 
+# ---------- add command ----------
 add_parser = subparsers.add_parser("add")
-add_parser.add_argument("-s", "--service", dest="service")
-add_parser.add_argument("-u", "--username", dest="username")
-add_parser.add_argument("-p", "--password", dest="password")
-add_parser.add_argument("-e", "--email", dest="email")
+add_parser.add_argument("-s", "--service", dest="service", required=True)
+add_parser.add_argument("-p", "--password", dest="password", required=True)
+
+username_group = add_parser.add_mutually_exclusive_group(required=True)
+username_group.add_argument("-u", "--username", dest="username")
+username_group.add_argument("--nousername", action="store_true", dest="no_username")
+
+email_group = add_parser.add_mutually_exclusive_group(required=True)
+email_group.add_argument("-e", "--email", dest="email")
+email_group.add_argument("--noemail", action="store_true", dest="no_email")
 
 args = parser.parse_args()
-if args.cmd = "add":
+if args.cmd == "add":
     add.add_password(
         service=args.service,
-        username=args.username,
         password=args.password,
+        username=args.username,
         email=args.email
     )
