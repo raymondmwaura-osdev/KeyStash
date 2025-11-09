c35a4fac619f7dbe9624e886ff98386b430860f0
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2025-11-09 17:03:18 +0300

    refactor: Chage 'add_password' function to 'AddCredentials' class.

--- src/features/add.py
+++ src/features/add.py
@@ -2,41 +2,39 @@ from src.utils import constants, storage
 from datetime import date
 import pathlib, json
 
-def add_password(service: str, password: str, username: str, email: str):
-    vault_encrypted = True
-    credentials = {
-        "service": service,
-        "password": password,
-        "username": username,
-        "email": email,
-        "date": date.today().strftime("%Y-%m-%d")
-    }
+class AddCredentials:
+    def __init__(
+        self,
+        service: str,
+        password: str,
+        username: str,
+        email: str
+    ):
+        vault_encrypted = True
+        credentials = {
+            "service": service,
+            "password": password,
+            "username": username,
+            "email": email,
+            "date": date.today().strftime("%Y-%m-%d")
+        }
 
-    if not constants.VAULT.exists():
-        # Initialize with an empty list.
-        vault_encrypted = False
+        if not constants.VAULT.exists():
+            # Initialize VAULT with an empty list.
+            vault_encrypted = False
+            constants.VAULT.parent.mkdir(parents=True, exist_ok=True)
+            constants.VAULT.write_text(
+                json.dumps([])
+            )
 
-        constants.VAULT.parent.mkdir(parents=True, exist_ok=True)
-        constants.VAULT.write_text(
-            json.dumps([])
+        vault_contents = storage.read_json(
+            file=constants.VAULT,
+            encrypted=vault_encrypted,
+            master_password=constants.MASTER_PASSWORD
         )
 
-    vault_contents = storage.read_json(
-        file=constants.VAULT,
-        encrypted=vault_encrypted,
-        master_password=constants.MASTER_PASSWORD
-    )
+        print(vault_contents)
 
-    # TODO: Check if duplicates exist.
-
-
-    vault_contents.append(credentials)
-    storage.write_json(
-        contents=vault_contents,
-        file=constants.VAULT,
-        encrypt=True,
-        master_password=constants.MASTER_PASSWORD
-    )
 
 def get_credentials(
     all_credentials: list[dict],
